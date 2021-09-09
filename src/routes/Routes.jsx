a35438117e6e13d21ffe3776cab4eea0a07e2ac5
MobinX <47663360+MobinX@users.noreply.github.com>
2021-09-09 22:05:34 +0600

    ggg

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Story from '@/pages/Story';
 import { Navbar } from '@/components/navbar';
-import { B } from '../../dist/assets/vendor.4cccd92b';
 import { Dashboard } from '@/pages/dashboard';
 
 export default function Routes() {
